ea28ba91a206d8828ee4c10eecb5324f0463c38a
im.budro <andreabudruni05@gmail.com>
2025-12-16 22:59:15 +0100

    rimossa la cartella del sito dai commit

--- src/main/java/http_server/Main.java
+++ src/main/java/http_server/Main.java
@@ -1,19 +1,18 @@
 package http_server;
 
-import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello world!");
-        ServerSocket ss = new ServerSocket(3000);
-        while (true) {
-            Socket s = ss.accept();
-            MioThread t = new MioThread(s);
-            t.start();
-            
+        try (ServerSocket ss = new ServerSocket(3000)) {
+            while (true) {
+                Socket s = ss.accept();
+                MioThread t = new MioThread(s);
+                t.start();
+                
+            }
         }
-        
     }
 }
